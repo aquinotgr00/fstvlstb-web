e6e40d583cb103063cab390142b74d34e5e6470e
Ibnu Nasikin <aquinotgr@pm.me>
2019-04-15 21:31:41 +0700

    add default string length for mysql support

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,8 @@ namespace App\Providers;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Validator;
 
+use Illuminate\Support\Facades\Schema;
+
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -21,6 +23,8 @@ class AppServiceProvider extends ServiceProvider
         Validator::replacer('phone_number', function($message, $attribute, $rule, $parameters) {
             return str_replace(':attribute',$attribute, ':attribute is invalid phone number');
         });
+
+        Schema::defaultStringLength(191);
     }
 
     /**
