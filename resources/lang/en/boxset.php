e74c16c0125f73aefdd1bbd78e6078f9cf0878a7
Rizky Rahardian Akbar <rahardian.rizky@gmail.com>
2019-04-16 14:51:57 +0700

    update lang

--- resources/lang/en/boxset.php
+++ resources/lang/en/boxset.php
@@ -18,7 +18,7 @@ return [
     'closed_pre_order' => 'CLOSED PRE-ORDER',
     'description'=>'This album is marketed in some sets of BOXSET containing souvenirs that you can get either as a pack
                     or separately. Taking BOXSET of FSTVLST II album, not only you share your support but also a piece of
-                    your idelaism to us.',
+                    your idealism to us.',
     'edition'=>'EDITION 01'
 
 ];
