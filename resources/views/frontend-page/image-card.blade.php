ca731e3807f914c053974059ac906808afd7721d
Rizky Rahardian Akbar <rahardian.rizky@gmail.com>
2019-05-17 22:56:45 +0700

    update twitter with image post

--- resources/views/frontend-page/image-card.blade.php
+++ resources/views/frontend-page/image-card.blade.php
@@ -2,7 +2,7 @@
 <head>
 	<meta name="twitter:card" content="summary_large_image">
 	<meta name="twitter:site" content="@fstvlst">
-	<meta name="twitter:title" content="">
+	<meta name="twitter:title" content="FSTVLST">
 	<meta name="twitter:description" content="">
 	<meta name="twitter:creator" content="@fstvlst">
 	<meta name="twitter:image" content="https://fstvlst-bucket-staging.s3.ap-southeast-1.amazonaws.com/uploads/5cdecc5fac11c-7B79A9A0-6FE5-41BE-B5C1-77FC1C565218.jpeg">
